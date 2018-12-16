3d432f4b4989ba68bfa53649136ee26853679cae
Aliyah Bond <ajbondmk@hotmail.co.uk>
2018-12-16 18:44:58 +0000

    Add list of all words in the descriptions

--- agents/representation_generator.py
+++ agents/representation_generator.py
@@ -2,6 +2,11 @@ import string
 
 class RepresentationGenerator():
 
+    def get_all_words(self):
+        f = open('all_words.txt', 'r')
+        self.all_words = f.read().split('\n')
+        f.close()
+
     def split_input(self, input_string):
         translator = str.maketrans('', '', string.punctuation)
         sanitised_description = input_string.translate(translator).lower()
