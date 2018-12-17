f26421b1cd56f5baeb5b5d8ff0d1fe0dac856593
Aliyah Bond <ajbondmk@hotmail.co.uk>
2018-12-17 16:25:51 +0000

    Create LSTM module

--- agents/representation_generator.py
+++ agents/representation_generator.py
@@ -9,12 +9,28 @@ class RepresentationGenerator():
         f = open('all_words.txt', 'r')
         self.all_words = f.read().split('\n')
         f.close()
+        
+        self.input_dim = len(self.all_words)
+        self.hidden_dim = 5   # Number of commands.
+        self.lstm = nn.LSTM(self.input_dim, self.hidden_dim)
 
     def split_input(self, input_string):
+        
+        # The axes semantics are (num_layers, minibatch_size, hidden_dim).
+        hidden = (torch.zeros(1, 1, self.hidden_dim), torch.zeros(1, 1, self.hidden_dim))
+        # Alternatively could use `torch.randn`.
+
         translator = str.maketrans('', '', string.punctuation)
         sanitised_description = input_string.translate(translator).lower()
         words = sanitised_description.split()
 
         for word in words:
-            word_vec = torch.zeros(len(self.all_words))
-            word_vec[self.all_words.index(word)] = 1
+            word_vec = torch.zeros(1,1,self.input_dim)
+            word_vec[0,0,self.all_words.index(word)] = 1
+
+            out, hidden = self.lstm(word_vec, hidden)
+            
+            print(out)
+            print(hidden)
+            print()
+    
