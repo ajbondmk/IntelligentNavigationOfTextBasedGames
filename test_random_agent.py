7a55e82ea8df9cbf9d1ffd861e538fc3897ad065
Aliyah Bond <ajbondmk@hotmail.co.uk>
2018-12-28 22:53:07 +0000

    Add file to test RandomAgent

--- test_random_agent.py
+++ test_random_agent.py
@@ -1,15 +1,15 @@
 import os
 import numpy as np
 import textworld
-from agent_02 import Agent02
+from random_agent import RandomAgent
 from debug_print import debug_print, debug_not_print
 
 
-""" This script trains Agent02 on all games in a world and outputs relevant statistics. """
+""" This script tests RandomAgent on all games in a world and outputs relevant statistics. """
 
 
 # Select world folder and locate its games.
-world_folder = "gen_games/twcc_easy_level3_gamesize10_step1_seed1_train-v0"
+world_folder = "gen_games/twcc_easy_level3_gamesize10_step1_seed1_train-v0-v0"
 envs = []
 for filename in os.listdir(world_folder):
     if filename.endswith(".ulx"):
@@ -22,13 +22,13 @@ max_moves = 50
 num_games = len(envs)
 
 # Number of times each game is played.
-num_repeats = 10
+num_repeats = 3
 
 # Initialise the arrays of move counts and scores.
 num_moves, scores = [], []
 
 # Create the agent.
-agent = Agent02(num_games * num_repeats)
+agent = RandomAgent()
 
 
 # Repeat each game multiple times.
@@ -46,36 +46,13 @@ for episode in range(num_repeats):
         # Reset the agent and environment.
         agent.reset(env)
         game_state = env.reset()
-        
-        # Initialise the list of room descriptions seen so far.
-        state_after = game_state.description
-        inputs_seen = [state_after]
 
         for num_step in range(max_moves):
         
             # Perform the action chosen by the agent.
             action = agent.act(game_state)
-            game_state, coin_reward, done = env.step(action)
-            
-            state_before = state_after
-            state_after = game_state.description
-
-            # Calculate the reward:
-            #    10 points if the agent picked up the coin.
-            #    1 point if the agent discovered a new room.
-            #    0 points otherwise.
-            reward = coin_reward * 10
-            if reward is 0:
-                if state_after not in inputs_seen:
-                    reward += 1
-                    inputs_seen.append(state_after)
-            debug_print("Reward:   {:d}".format(reward))
-            
-            # Add the transition to memory.
-            agent.memory.add_item(state_before, action, state_after, reward)
-
-            # Train the agent.
-            agent.optimise()
+            game_state, _, done = env.step(action)
+            debug_print(action)
 
             # If the game is finished (completed or failed), break.
             if done:
