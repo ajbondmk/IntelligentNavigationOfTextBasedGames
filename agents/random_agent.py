4642b1eb4fa06185e2693420e1b492c0e825b302
Aliyah Bond <ajbondmk@hotmail.co.uk>
2018-12-28 17:19:49 +0000

    Minor change to RandomAgent

--- agents/random_agent.py
+++ agents/random_agent.py
@@ -7,13 +7,9 @@ class RandomAgent(textworld.Agent):
 
     commands = ['go north', 'go east', 'go south', 'go west', 'take coin']
 
-    def __init__(self):
-        self.rng = random.Random()
-
     def reset(self, env):
         env.activate_state_tracking()
         env.compute_intermediate_reward()
 
-    def act(self, game_state, reward, done):
-        return self.rng.choice(self.commands)
-    
+    def act(self, game_state):
+        return random.choice(self.commands)
